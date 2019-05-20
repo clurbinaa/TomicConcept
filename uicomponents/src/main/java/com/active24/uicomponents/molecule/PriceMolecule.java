c0d5d741df0ca101afef2d854e087733126435a3
Christian Urbina <clurbinaa@liverpool.com.mx>
2019-05-20 15:17:50 -0500

    texts in priceMolecule

--- uicomponents/src/main/java/com/active24/uicomponents/molecule/PriceMolecule.java
+++ uicomponents/src/main/java/com/active24/uicomponents/molecule/PriceMolecule.java
@@ -8,7 +8,6 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.active24.uicomponents.R;
-import com.active24.uicomponents.atom.AtomicTextView;
 
 public class PriceMolecule extends LinearLayout {
 
@@ -17,10 +16,10 @@ public class PriceMolecule extends LinearLayout {
     private TextView regularPrice;
     private TextView transferPrice;
     private Type type = Type.NORMAL;
-    private String normal;
-    private String promotion;
-    private String regular;
-    private String transfer;
+    private CharSequence normal;
+    private CharSequence promotion;
+    private CharSequence regular;
+    private CharSequence transfer;
 
 
     public enum Type {
@@ -29,13 +28,15 @@ public class PriceMolecule extends LinearLayout {
         TRANSFER(2);
 
         private int type;
-        Type(int type){
+
+        Type(int type) {
             this.type = type;
         }
 
         public int getType() {
             return type;
         }
+
         static PriceMolecule.Type fromId(int id) {
             for (PriceMolecule.Type f : values()) {
                 if (f.type == id)
@@ -57,31 +58,41 @@ public class PriceMolecule extends LinearLayout {
 
     private void init(Context context, AttributeSet attrs) {
         inflate(context, R.layout.molecule_price, this);
-        int[] sets = {R.attr.normalPrice, R.attr.regularPrice, R.attr.promotionPrice,R.attr.transferPrice};
+        int[] sets = {R.attr.normalPrice, R.attr.regularPrice, R.attr.promotionPrice, R.attr.transferPrice};
         TypedArray typedArray = context.getTheme().obtainStyledAttributes(
-                attrs, R.styleable.AtomicTextView, 0, 0);
+                attrs, R.styleable.MoleculePrice, 0, 0);
         type = PriceMolecule.Type.fromId(typedArray.getInt(R.styleable.MoleculePrice_price_type, Type.NORMAL.getType()));
+        normal = typedArray.getText(R.styleable.MoleculePrice_normalPrice);
+        promotion = typedArray.getText(R.styleable.MoleculePrice_promotionPrice);
+        regular = typedArray.getText(R.styleable.MoleculePrice_regularPrice);
+        transfer = typedArray.getText(R.styleable.MoleculePrice_transferPrice);
         initComponents();
         setTypeView();
     }
 
     private void setTypeView() {
-        switch (type){
+        switch (type) {
             case NORMAL:
-                normalPrice.setText("");
                 normalPrice.setVisibility(VISIBLE);
+                if (normal != null)
+                    normalPrice.setText(normal);
                 promotionPrice.setVisibility(GONE);
                 regularPrice.setVisibility(GONE);
                 break;
             case DISCOUNT:
-                promotionPrice.setText("");
-                regularPrice.setText("");
+                if (promotion != null && regular != null) {
+                    promotionPrice.setText(promotion);
+                    regularPrice.setText(regular);
+                }
                 normalPrice.setVisibility(GONE);
                 promotionPrice.setVisibility(VISIBLE);
                 regularPrice.setVisibility(VISIBLE);
                 break;
             case TRANSFER:
-                normalPrice.setText("");
+                if (normal != null && transfer != null) {
+                    normalPrice.setText(normal);
+                    transferPrice.setText(transfer);
+                }
                 normalPrice.setVisibility(VISIBLE);
                 promotionPrice.setVisibility(GONE);
                 regularPrice.setVisibility(GONE);
